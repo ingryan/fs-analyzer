0e0a3bfddd2334cb4098ca0f965d996213a2fba2
Ryan Ingram <ryan44@mail.usf.edu>
2016-07-02 14:17:45 -0400

    Improved program execution
    Changed bytecounts to megabytes, fixed bug causing correct results not to be found

--- fs-analyzer.py
+++ fs-analyzer.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def scan_fs(*args):
+def scan_fs(excludelist, *args):
     #topdir=args[0]
     topdir="C:/"
 
@@ -12,10 +12,12 @@ def scan_fs(*args):
                                                 topdown=True,
                                                 onerror=print_error,
                                                 followlinks=False):
-        newsum = 0
+
+        dirnames[:] = [x for x in dirnames if x not in excludelist]
+        dirsum = 0
         for name in filenames:
             try:
-                newsum += os.path.getsize(os.path.join(dirpath, name))
+                dirsum += os.path.getsize(os.path.join(dirpath, name))
             except (PermissionError, OSError) as err:
                 print(str(err))
                 continue
@@ -26,16 +28,24 @@ def scan_fs(*args):
                 print("File not found, skipping")
 
 
-        if newsum>100000000:
-            print("{0} has size {1}B".format(dirpath, newsum))
+        if dirsum>100000000:
+            mbdirsum = dirsum // 10**6
+            print("{0} has size {1}MB".format(dirpath, mbdirsum))
 
-        if newsum>maxsize:
-            maxsize = newsum
+        if dirsum>maxsize:
+            maxsize = dirsum
             fattestdir = dirpath
 
-    print("{0} is the winner with size {1}B".format(fattestdir, maxsize))
+    print("{0} is the winner with size {1}MB".format(fattestdir, maxsize//10**6))
 
 def print_error(exceptionobject):
     print(str(exceptionobject))
 
-scan_fs()
+exclusions = {'Windows',
+            'ProgramData',
+            'C:/Users/Default',
+            'AppData'
+            }
+
+
+scan_fs(exclusions)
