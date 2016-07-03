0893f59c970e0aa29cc55eb61d018d598cb8ab1d
Ryan Ingram <ryan44@mail.usf.edu>
2016-07-03 01:12:10 -0400

    Added results logfile functionality
    Wrote a function which writes the results of the scan to a file. The results show a list of directories larger than 100 MB sorted in descending order by size.

--- fs-analyzer.py
+++ fs-analyzer.py
@@ -1,18 +1,22 @@
 import os
 import sys
 
+from collections import OrderedDict
+
 def scan_fs(excludelist, *args):
     #topdir=args[0]
-    topdir="C:/"
+    topdir="C:\\"
 
     maxsize = 0
     fattestdir = "C:"
+
+    results = {}
+    resultscutoff = 10**8
 
     for dirpath, dirnames, filenames in os.walk(topdir,
                                                 topdown=True,
                                                 onerror=print_error,
                                                 followlinks=False):
-
         dirnames[:] = [x for x in dirnames if x not in excludelist]
         dirsum = 0
         for name in filenames:
@@ -28,18 +32,38 @@ def scan_fs(excludelist, *args):
                 print("File not found, skipping")
 
 
-        if dirsum>100000000:
-            mbdirsum = dirsum // 10**6
-            print("{0} has size {1}MB".format(dirpath, mbdirsum))
+        if dirsum>resultscutoff:
+            dirsum = dirsum // 10**6 #stored as megabytes
+            print("{0} has size {1}MB".format(dirpath, dirsum))
+            results[dirpath]=dirsum
 
-        if dirsum>maxsize:
-            maxsize = dirsum
-            fattestdir = dirpath
+            if dirsum>maxsize:
+                maxsize = dirsum #also stored as megabytes
+                fattestdir = dirpath
 
-    print("{0} is the winner with size {1}MB".format(fattestdir, maxsize//10**6))
+    print("{0} is the winner with size {1}MB".format(fattestdir, maxsize))
+    return results
 
 def print_error(exceptionobject):
     print(str(exceptionobject))
+
+def print_results(outdict, filename=None):
+    if not filename:
+        filename = os.getcwd()+"/logs/output.txt"
+
+    if not os.path.exists(os.path.dirname(filename)):
+        os.mkdir(os.path.dirname(filename))
+
+    sorted_output = OrderedDict(sorted(outdict.items(), key=lambda x: x[1], reverse=True))
+    print("Writing to {0}".format(filename))
+
+    with open(filename, 'w+') as log:
+        for path in sorted_output:
+            log.write("{0}: {1}MB \n".format(path, sorted_output[path]))
+
+
+
+
 
 exclusions = {'Windows',
             'ProgramData',
@@ -47,5 +71,5 @@ exclusions = {'Windows',
             'AppData'
             }
 
-
-scan_fs(exclusions)
+pathdict = scan_fs(exclusions)
+print_results(pathdict, filename='C:/proglogs/out.txt')
